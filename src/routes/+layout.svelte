06f53e4e62d083172cea4f8ac78d2d1309fe56f6
haag08 <121289452+haag08@users.noreply.github.com>
2026-05-04 09:56:53 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import TabBar from '$lib/components/TabBar.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<div class="min-h-screen pb-28">
+	{@render children()}
+</div>
+
+<TabBar />
